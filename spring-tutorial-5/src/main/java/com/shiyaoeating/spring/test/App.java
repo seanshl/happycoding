374ea4649a6bf1a0c4c858033d1aa7625813eebc
liushiyao <liushiyao@192.168.1.5>
2016-02-14 11:45:28 -0600

    using classpathxml appication context

--- spring-tutorial-5/src/main/java/com/shiyaoeating/spring/test/App.java
+++ spring-tutorial-5/src/main/java/com/shiyaoeating/spring/test/App.java
@@ -1,16 +1,17 @@
 package com.shiyaoeating.spring.test;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("/com/shiyaoeating/spring/test/beans/beans.xml");
 		
 		//Person person = new Person();
 		Person person = (Person)context.getBean("person");
 		person.speak();
+		((ClassPathXmlApplicationContext) context).close();
 	}
 
 }
